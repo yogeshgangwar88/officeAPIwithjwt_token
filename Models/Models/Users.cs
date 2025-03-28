374000fd8f93bcf9898abe55947e44896cfa24a5
yogeshgangwar88 <yogesh.gangwar88@gmail.com>
2025-03-28 12:59:29 +0530

    added views with standard controller in this project

--- Models/Models/Users.cs
+++ Models/Models/Users.cs
@@ -14,18 +14,34 @@ namespace Models.Models
         [Key]
         public int UserID { get; set; }
         [Required]
+        [MinLength(4, ErrorMessage = "Username cannot less than 4 char")]
         public string? Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        [DataType(DataType.Date)]
+        [CustomValidation(typeof(Users), nameof(ValidateDOB))]
         public DateTime? DOB { get; set; }
         [Required]
         public int Age { get; set; }
-        [Required]
         public bool Ismarried { get; set; }
+        [Required]
         public string? Password { get; set; }
-      
+
         //// Navigation Property (One-to-One)
         public UserFamilyDetails? UserFamilyDetails { get; set; }
         //navigation property for many to many ///
         public List<UserProducts>? UserProducts { get; set; }
+
+        public static ValidationResult? ValidateDOB(DateTime? dob, ValidationContext context)
+        {
+            if (dob.HasValue)
+            {
+                if (dob.Value > DateTime.Today)
+                {
+                    return new ValidationResult("Date of Birth cannot be in the future.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+
     }
 }
---
+++ officeapi/Controllers/NonAPIController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Models;
+using RepoLibrary.DBContext;
+
+namespace officeapi.Controllers
+{
+    public class NonAPIController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public NonAPIController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        [Route("/NonAPIHome")]
+        public IActionResult NonAPIHome()
+        {
+            ViewData["Title"] = "Add";
+            return View(new Users());
+        }
+        [HttpPost]
+        [Route("/NonAPIHome")]
+        public async Task< IActionResult> NonAPIHome(Users user)
+        {
+            if (ModelState.IsValid) {
+                _context.Users.Add(user);
+                await   _context.SaveChangesAsync();
+                TempData["success"] = "Add Successfully";
+                return RedirectToAction("NonAPIHome");
+            }
+            else
+            {
+                TempData["error"] = "Add Failed";
+                return View(user);
+            }
+
+        }
+    }
+}
